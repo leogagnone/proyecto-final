0e2cba4b58ce215f5079ddf4630aba8c3b8ba8f3
leogagnone <71908339+leogagnone@users.noreply.github.com>
2021-06-03 17:53:23 -0300

    Update index.ts
    Conexión con la base de datos.

--- index.ts
+++ index.ts
@@ -1,12 +1,29 @@
-import Server from "./class/server";
+import Server from './class/server';
+import mysql from 'mysql';
 
-
-
-
-
-//Creando servidor web
+//Server web
 const server = new Server();
 
 server.start(()=>{
-    console.log(`Server running on port ${server.puerto} and host ${server.host}`);
+    console.log(`Server running on port ${server.puerto} on host ${server.host}`);
+});
+
+//Database connect
+const connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"gestion_inmobiliaria",
+    port:3306
+
+})
+
+connection.connect((error)=>{
+    if (error){
+        throw error
+    }
+    else{
+       console.log(`App connect to MySQL data base`) 
+    }
+
 });
